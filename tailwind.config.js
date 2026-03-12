496d797698ea714e57d13e368b7033d086800f42
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-12 14:45:07 +0300

    Реализована секция Header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'source-sans-pro': ['Source Sans Pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
