d8b0951c59ab6af4667b397a406f26a82d90604a
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-15 16:43:02 +0300

    Скругление кнопок, изменен шрифт цен, меньше ширина элементов, горизонтальный скролл цен

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,12 +10,10 @@ export default function Hero(){
     return(
         <section className="min-h-190 max-md:min-h-screen max-md:h-auto bg-[#17191A] bg-[url('/images/bg.svg')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center max-md:justify-start px-15 max-md:px-4 max-md:pt-4 max-md:pb-8">            <div className = "flex flex-row max-md:flex-col items-center justify-center max-md:text-center max-md:gap-8">
 
-                {/*Текст слева*/}
                 <div className="max-md:order-1" ref={ref}>
 
                     <img src="/images/hero-2.png" alt="hero image" className = "max-w-125 max-h-125 max-md:max-w-80 max-md:max-h-80 max-md:mx-auto md:hidden float"/>
 
-                    {/*Шильдик*/}
                     <motion.span 
                         initial={{opacity:0, y:20}}
                         animate={inView ? {opacity:1, y:0} : {opacity:0, y:20}}
@@ -29,7 +27,6 @@ export default function Hero(){
 
                     </motion.span>
 
-                    {/*Заголовок*/}
                     <motion.h1 
                         initial={{opacity:0, y:30}}
                         animate={inView ? {opacity:1, y:0} : {opacity:0, y:30}}
@@ -38,7 +35,6 @@ export default function Hero(){
 
                     <div className = "w-full flex items-center justify-center mb-3.5 max-md:mb-4">
                         <svg width="547" height="2" viewBox="0 0 547 2" fill="none" xmlns="http://www.w3.org/2000/svg" className="max-md:w-80">
-                            <line x1="1" y1="1" x2="546" y2="1" stroke="url(#paint0_linear_26324_553)" strokeWidth="2" strokeLinecap="round" strokeDasharray="6 6"/>
                             <defs>
                                 <linearGradient id="paint0_linear_26324_553" x1="7.64151e-07" y1="2.5" x2="547" y2="2.50039" gradientUnits="userSpaceOnUse">
                                     <stop stopColor="#9AC321" stopOpacity="0.1"/>
@@ -51,7 +47,6 @@ export default function Hero(){
 
 
 
-                    {/*Описание*/}
                     <motion.p 
                         initial={{opacity:0, y:30}}
                         animate={inView ? {opacity:1, y:0} : {opacity:0, y:30}}
@@ -59,8 +54,6 @@ export default function Hero(){
                         className = "font-source-sans-pro font-normal text-xl max-md:text-lg leading-8 text-[#ADB3BF] mb-20 max-md:mb-8">It is a long established fact that a reader will be distracted by the readable content of a page when
                         looking at its layout.</motion.p>
 
-
-                    {/*Кнопки*/}
                     <div className = "flex flex-row max-md:flex-col items-center gap-7.5 max-md:gap-4 max-md:w-full">
                         <button className = "px-11 h-14 w-48 max-md:w-full max-md:h-12 bg-[#1B70F1] rounded-full max-md:max-w-50 hover:bg-[#1E1F25] border border-[#1B70F1]">
                             <a href="#" className = "font-source-sans-pro font-normal text-xl max-md:text-base leading-8 text-white">Start Now</a>
