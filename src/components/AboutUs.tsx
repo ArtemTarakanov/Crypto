77a75106da778c7b2406c29e569df143b634702a
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-15 15:16:55 +0300

    Обновлена секция About Us

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,7 +6,7 @@ export default function AboutUs() {
             <div className = "flex flex-col gap-15 items-center max-md:gap-7.5">
                 <div className = "flex flex-row items-center gap-25 max-md:flex-col max-md:gap-4">
                     <h2 className="font-source-pro font-bold text-[58px] text-white max-w-150 max-md:text-4xl max-md:max-w-116 max-md:text-center">What Traders Saying About Us! </h2>
-                    <p className = "font-source-pro font-normal text-lg leading-7 text-[#ADB3BF] max-w-260 max-md:text-lg max-md:max-w-100 max-md:text-center">Lorem Ipsum has been the industry's standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled type specimen book.</p>
+                    <p className = "font-source-pro font-normal text-lg leading-7 text-[#ADB3BF] max-w-260 max-md:text-lg max-md:max-w-90 max-md:text-center">Lorem Ipsum has been the industry's standard dummy text since the 1500s, when an unknown printer took a galley of type and scrambled type specimen book.</p>
                 </div>
 
                 <svg width="80%" height="1" viewBox="0 0 1590 1" fill="none" xmlns="http://www.w3.org/2000/svg" className="max-md:max-w-[50%] ">
